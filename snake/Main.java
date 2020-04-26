00ec42267e965475ebe3b9d2e4416efc5f9db1d6
baoanh1310 <dataholicguy@gmail.com>
2020-04-26 09:23:32 +0700

    Create a simple Snake game

---
+++ snake/Main.java
@@ -0,0 +1,213 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+import javafx.animation.AnimationTimer;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main extends Application {
+
+    static int speed = 5;
+    static int foodColor = 0;
+    static int width = 20;
+    static int height = 20;
+    static int foodX = 0;
+    static int foodY = 0;
+    static int cornerSize = 25;
+    static List<Corner> snake = new ArrayList<Corner>();
+    static Dir direction = Dir.left;
+    static boolean gameOver = false;
+    static Random rand = new Random();
+
+    // directions for the controls
+    public enum Dir {
+        left, right, up, down;
+    }
+
+    // squares of which the snake constists
+    public static class Corner {
+        int x;
+        int y;
+
+        public Corner(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+	@Override
+	public void start(Stage primaryStage) {
+        try {
+            newFood();
+            // Create a VBox and a Canvas as background
+            VBox root = new VBox();
+            Canvas c = new Canvas(width*cornerSize, height*cornerSize);
+
+            // GraphicsContext to paint the snake
+            GraphicsContext gc = c.getGraphicsContext2D();
+
+            root.getChildren().add(c);
+
+            // flip book
+            new AnimationTimer() {
+                long lastTick = 0;
+                public void handle(long now) {
+                    if (lastTick == 0) {
+                        lastTick = now;
+                        tick(gc);
+                        return;
+                    }
+                    // 1 mio ticks = new frame every 'speed' second
+                    if (now - lastTick > 1000000000 / speed) {
+                        lastTick = now;
+                        tick(gc);
+                    }
+                }
+            }.start();
+
+            Scene scene = new Scene(root, width*cornerSize, height*cornerSize);
+
+            // Control 
+            // Set the keyboard controls to W, A, S, D
+            scene.addEventFilter(KeyEvent.KEY_PRESSED, key -> {
+                if (key.getCode() == KeyCode.UP) {
+                    direction = Dir.up;
+                } else if (key.getCode() == KeyCode.LEFT) {
+                    direction = Dir.left;
+                } else if (key.getCode() == KeyCode.DOWN) {
+                    direction = Dir.down;
+                } else if (key.getCode() == KeyCode.RIGHT) {
+                    direction = Dir.right;
+                }
+            });
+
+            // Set the snake to 3 parts (Corners) at the beginning
+            snake.add(new Corner(width/2, height/2));
+            snake.add(new Corner(width/2, height/2));
+            snake.add(new Corner(width/2, height/2));
+
+            // Optinal css style
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("SNAKE GAME");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }    
+	}
+
+    public static void tick(GraphicsContext gc) {
+        if (gameOver) {
+            gc.setFill(Color.RED);
+            gc.setFont(new Font("", 50));
+            gc.fillText("GAME OVER", 100, 250);
+            return;
+        }
+
+        for (int i = snake.size()-1; i >= 1; i--) {
+            snake.get(i).x = snake.get(i-1).x;
+            snake.get(i).y = snake.get(i-1).y;
+        }
+
+        switch(direction) {
+            case up:
+                snake.get(0).y--;
+                if (snake.get(0).y < 0) {
+                    gameOver = true;
+                }
+                break;
+            case down:
+                snake.get(0).y++;
+                if (snake.get(0).y > height) {
+                    gameOver = true;
+                }
+                break;
+            case left:
+                snake.get(0).x--;
+                if (snake.get(0).x < 0) {
+                    gameOver = true;
+                }
+                break;
+            case right:
+                snake.get(0).x++;
+                if (snake.get(0).x > width) {
+                    gameOver = true;
+                }
+                break;
+        }
+        // Eat food - snake grow
+        if (foodX == snake.get(0).x && foodY == snake.get(0).y) {
+            snake.add(new Corner(-1, -1));
+            newFood();
+        }
+
+        // Self destroy
+        for (int i = 1; i < snake.size(); i++) {
+            if (snake.get(0).x == snake.get(i).x && snake.get(0).y == snake.get(i).y) {
+                gameOver = true;
+            }
+        }
+
+        // Fill the background
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, width*cornerSize, height*cornerSize);
+
+        // Score
+        gc.setFill(Color.WHITE);
+        gc.setFont(new Font("", 30));
+        gc.fillText("Score: " +(speed-6), 10, 30);
+
+        // Random food color
+        Color cc = Color.WHITE;
+        switch (foodColor) {
+            case 0: cc = Color.PURPLE; break;
+            case 1: cc = Color.ORANGE; break;
+            case 2: cc = Color.PINK; break;
+            case 3: cc = Color.YELLOW; break;
+        }
+        gc.setFill(cc);
+        gc.fillOval(foodX * cornerSize, foodY * cornerSize, cornerSize, cornerSize);
+
+        // snake
+        for (Corner c: snake) {
+            gc.setFill(Color.LIGHTGREEN);
+            gc.fillRect(c.x * cornerSize, c.y * cornerSize, cornerSize-1, cornerSize-1);
+            gc.setFill(Color.GREEN);
+            gc.fillRect(c.x * cornerSize, c.y * cornerSize, cornerSize-2, cornerSize-2);
+        }
+    }
+
+    
+
+    // Food
+    public static void newFood() {
+        // place a food on random location on the canvas (if there is no snake)
+        start: while (true) {
+                foodX = rand.nextInt(width);
+                foodY = rand.nextInt(height);
+
+                for (Corner c: snake) {
+                    if (c.x == foodX && c.y == foodY) {
+                        continue start;
+                    }
+                }
+
+            // choose a new color for food and increase the speed of snake
+            foodColor = rand.nextInt(5);
+            speed++;
+            break;
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
